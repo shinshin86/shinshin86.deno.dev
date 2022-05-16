0d0311c147fc2bd316633eb397991b6c6be530dd
shinshin86 <beagles1986@gmail.com>
2022-05-17 08:01:47 +0900

    Fix text

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World! - shinshin86", {
+  const response = new Response("Hello! - shinshin86", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
